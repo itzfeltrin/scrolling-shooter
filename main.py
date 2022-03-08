2e12a4e1a4eb3fe087511ff567f6cfd444b06139
Pedro Feltrin <pedrofeltrin45@gmail.com>
2022-03-08 11:09:08 -0300

    add idle/running/jumping animations

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ pygame.init()
 # define game constants
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
+GRAVITY = 0.75
 
 # create pygame screen
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -22,23 +23,44 @@ moving_right = False
 
 # define colors
 BG = (144, 201, 120)
+RED = (255, 0, 0)
 
 
 def draw_bg():
     screen.fill(BG)
+    pygame.draw.line(screen, RED, (0, 300), (SCREEN_WIDTH, 300))
 
 
 # define soldier class
 class Soldier(pygame.sprite.Sprite):
     def __init__(self, char_type, x, y, scale, speed):
         pygame.sprite.Sprite.__init__(self)
+        self.alive = True
         self.char_type = char_type
         self.speed = speed
         # -1 = left, 1 = right
         self.direction = 1
+        self.vel_y = 0
+        self.jump = False
         self.flip = False
-        img = pygame.image.load(f'assets/img/{self.char_type}/Idle/0.png')
-        self.img = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
+        self.animation_list = []
+        # frame_index is where the animation is at
+        self.frame_index = 0
+        self.action = 0
+        self.update_time = pygame.time.get_ticks()
+        temp_list = []
+        for i in range(5):
+            img = pygame.image.load(f'assets/img/{self.char_type}/Idle/{i}.png')
+            img = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
+            temp_list.append(img)
+        self.animation_list.append(temp_list)
+        temp_list = []
+        for i in range(6):
+            img = pygame.image.load(f'assets/img/{self.char_type}/Run/{i}.png')
+            img = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
+            temp_list.append(img)
+        self.animation_list.append(temp_list)
+        self.img = self.animation_list[self.action][self.frame_index]
         self.rect = self.img.get_rect()
         self.rect.center = (x, y)
 
@@ -57,8 +79,42 @@ class Soldier(pygame.sprite.Sprite):
             self.flip = False
             self.direction = 1
 
+        # jump
+        if self.jump is True:
+            self.vel_y = -11
+            self.jump = False
+
+        # apply gravity
+        self.vel_y += GRAVITY
+        if self.vel_y >= 10:
+            self.vel_y = 10
+        dy += self.vel_y
+
+        if self.rect.bottom + dy > 300:
+            dy = 300 - self.rect.bottom
+
         self.rect.x += dx
         self.rect.y += dy
+
+    def update_animation(self):
+        cooldown = 100
+        # update image depending on current frame
+        self.img = self.animation_list[self.action][self.frame_index]
+        # check if enough time has passed since the last update
+        if pygame.time.get_ticks() - self.update_time > cooldown:
+            self.update_time = pygame.time.get_ticks()
+            self.frame_index += 1
+            # if animation has run out reset frame_index
+            if self.frame_index >= len(self.animation_list[self.action]):
+                self.frame_index = 0
+
+    def update_action(self, new_action):
+        # check if the new action is different to the previous one
+        if new_action != self.action:
+            self.action = new_action
+            # reset frame_index
+            self.frame_index = 0
+            self.update_time = pygame.time.get_ticks()
 
     def draw(self):
         screen.blit(pygame.transform.flip(self.img, self.flip, False), self.rect)
@@ -75,10 +131,20 @@ while run:
 
     # redraw background on every cycle of the loop
     draw_bg()
-    # move and draw player on screen
-    player.move(moving_left, moving_right)
+
+    player.update_animation()
     player.draw()
     enemy.draw()
+
+    if player.alive:
+        # update player actions
+        if moving_left or moving_right:
+            player.update_action(1)
+        else:
+            player.update_action(0)
+
+    # move and draw player on screen
+    player.move(moving_left, moving_right)
 
     # loop through events on every cycle of the loop
     for event in pygame.event.get():
@@ -93,6 +159,8 @@ while run:
                 moving_left = True
             if event.key == pygame.K_d:
                 moving_right = True
+            if event.key == pygame.K_w and player.alive:
+                player.jump = True
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_a:
                 moving_left = False
